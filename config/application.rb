8ac6d1317107f3cf0411312ab8aab2fdf5250584
ASTM <astm_desig@hotmail.com>
2015-12-24 14:57:38 +0200

    allow the cros requestes

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module Ionic
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+    #  define the cross requestes
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :put, :delete, :post, :options]
+      end
+    end
+
+    
   end
+
 end
